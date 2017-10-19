8becc6a1fa213eb763634e1711bd3cdc8ba88c5b
40172641 <40172641@live.napier.ac.uk>
2017-10-19 18:08:55 +0000

    Update

--- sourcecode/coursework.py
+++ sourcecode/coursework.py
@@ -16,6 +16,6 @@ def album():
 @app.errorhandler(404)
 def page_not_found(error):
   return render_template('error.html'), 404
-
+#...
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
