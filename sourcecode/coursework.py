47e51035a8d14a87ff3dfaf9da20a0fbc17ddda0
40172641 <40172641@live.napier.ac.uk>
2017-10-19 19:43:11 +0000

    Pablo Honey page added

--- sourcecode/coursework.py
+++ sourcecode/coursework.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,6 +16,16 @@ def album():
 @app.errorhandler(404)
 def page_not_found(error):
   return render_template('error.html'), 404
+
+@app.route('/albums/pablohoney/')
+def pablo():
+  return render_template('pablo_honey.html'), 200
+
+#@app.route('/albums/inrainbows/')
+#def pablo(name=None):
+ # song_name = 'Pablo Honey'
+  #return render_template('song.html', song_name=song_name)
+
 #...
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
