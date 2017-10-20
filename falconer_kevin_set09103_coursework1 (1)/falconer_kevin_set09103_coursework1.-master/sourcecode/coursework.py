01aafbacbac14e0aa84bee9e1e6e2b2ec9ca71ea
40172641 <31956077+40172641@users.noreply.github.com>
2017-10-20 20:08:50 +0100

    Add files via upload

---
+++ falconer_kevin_set09103_coursework1 (1)/falconer_kevin_set09103_coursework1.-master/sourcecode/coursework.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('home.html'), 200
+
+@app.route('/info/')
+def info():
+  return render_template('info.html'), 200
+
+@app.route('/albums/')
+def album():
+  return render_template('album.html'), 200
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('error.html'), 404
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
