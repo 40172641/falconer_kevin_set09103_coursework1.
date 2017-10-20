6eeb1552b839796ed6c7e96f40d084c2bbea714d
40172641 <40172641@live.napier.ac.uk>
2017-10-20 04:52:35 +0000

    JSON testing

--- sourcecode/coursework.py
+++ sourcecode/coursework.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, json, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
   return render_template('home.html'), 200
+
+@app.route('/albums/<album_name>/')
+def root(album_name):
+  jsonfile=open('album.json').read()
+  album=json.loads(jsonfile)
+  return render_template("album.html", albums=album)
 
 @app.route('/info/')
 def info():
@@ -11,21 +17,20 @@ def info():
 
 @app.route('/albums/')
 def album():
-  return render_template('album.html'), 200
+  return render_template('albums.html'), 200
+
+@app.route("/contact/", methods=['POST', 'GET'])
+def contact():
+  if request.method == 'POST':
+    print request.form
+    names= request.form['name']
+    return render_template('response.html', name=names), 200
+  else:
+    return render_template('contact.html'),200
 
 @app.errorhandler(404)
 def page_not_found(error):
   return render_template('error.html'), 404
 
-@app.route('/albums/pablohoney/')
-def pablo():
-  return render_template('pablo_honey.html'), 200
-
-#@app.route('/albums/inrainbows/')
-#def pablo(name=None):
- # song_name = 'Pablo Honey'
-  #return render_template('song.html', song_name=song_name)
-
-#...
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
